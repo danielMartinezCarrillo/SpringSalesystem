8050fb13f7bf00732f928b5939d9a60424b6f88b
JorgePedraza2019 <jorge_luis.2008@hotmail.com>
2020-03-25 20:45:50 -0600

    Commit inicial

---
+++ src/main/java/com/jorge/tecmilenio/salesystem/SpringSalesystemApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.tecmilenio.salesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSalesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSalesystemApplication.class, args);
+	}
+
+}
